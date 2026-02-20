f48ad6d772fb3ea4ae36c34d177452b2a81714ab
Kristine Kopce <kristine.kopce@gmail.com>
2026-02-20 12:40:51 +0200

    aprekins stundas

--- converter.py
+++ converter.py
@@ -1,3 +1,17 @@
 minutes = 20
 hours = minutes / 60
-print(f"{minutes} minūtes ir {minutes / 60:.2f} stundas")
+import math
+# Konstante
+MINUTES_PER_HOUR = 60
+
+try:
+    # Lietotāja izvēle
+    minutes = int(input("Ievadi minūtes: "))
+    # Aprēķins stundas
+    hours = minutes / MINUTES_PER_HOUR
+    # Nopaļošana uz augsu 2 cipari aiz komata
+    hours_ceiled = math.ceil(hours * 100) / 100
+    #rezultāta izvadīšana ar f-string
+    print(f"{minutes} minūtes ir {hours_ceiled:.2f} stundas")
+except ValueError:
+    print("Kļūda: Lūdzu, ievadi derīgu minūšu skaitu.")
